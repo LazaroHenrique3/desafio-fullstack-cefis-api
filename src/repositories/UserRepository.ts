88e0b76387e176b0a630974d51830ed0348fbb06
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-24 16:22:34 -0300

    feat: Criando e implementando o UserRepository

---
+++ src/repositories/UserRepository.ts
@@ -0,0 +1,119 @@
+import { User } from '@prisma/client'
+import { IUserRepository } from '../interfaces/IUserRepository'
+import { prisma } from '../database/PrismaClientInstance'
+import { checkIfUserExists } from './utils/checkFunctions'
+
+class UserRepository implements IUserRepository {
+
+    public async create(name: string, role: 'STUDENT' | 'TEACHER'): Promise<User | Error> {
+        try {
+            const newUser = await prisma.user.create({
+                data: {
+                    name,
+                    role
+                }
+            })
+
+            return newUser
+        } catch (error) {
+            console.error(error)
+            return new Error('Erro ao criar registro.')
+        }
+    }
+
+    public async list(page: number, limit: number, filter: string, orderBy: 'asc' | 'desc'): Promise<User[] | Error> {
+        try {
+            const users = await prisma.user.findMany({
+                skip: (page - 1) * limit,
+                take: limit,
+                where: {
+                    name: {
+                        contains: filter
+                    }
+                },
+                orderBy: {
+                    name: orderBy
+                }
+            })
+
+            return users
+        } catch (error) {
+            console.error(error)
+            return new Error('Erro ao listar registro.')
+        }
+    }
+
+    public async count(filter: string): Promise<number | Error> {
+        try {
+            const countOfUsers = await prisma.user.count({
+                where: {
+                    name: {
+                        contains: filter
+                    }
+                }
+            })
+
+            return countOfUsers
+        } catch (error) {
+            console.error(error)
+            return new Error('Erro ao receber quantidade de registros.')
+        }
+    }
+
+    public async getById(idUser: number): Promise<User | null | Error> {
+        try {
+            const user = await prisma.user.findUnique({
+                where: {
+                    id: idUser
+                }
+            })
+
+            return user
+        } catch (error) {
+            console.error(error)
+            return new Error('Erro ao buscar registro.')
+        }
+    }
+
+    public async delete(idUser: number): Promise<void | null | Error> {
+        try {
+            const idExists = await checkIfUserExists(idUser)
+            if (!idExists) return null
+
+            await prisma.user.delete({
+                where: {
+                    id: idUser
+                }
+            })
+
+
+        } catch (error) {
+            console.error(error)
+            return new Error('Erro ao excluir registro.')
+        }
+    }
+
+    public async update(idUser: number, name: string): Promise<User | null | Error> {
+        try {
+            const idExists = await checkIfUserExists(idUser)
+            if (!idExists) return null
+
+            const updatedUser = await prisma.user.update({
+                where: {
+                    id: idUser
+                },
+                data: {
+                    name,
+                }
+            })
+
+            return updatedUser
+        } catch (error) {
+            console.error(error)
+            return new Error('Erro ao atualizar registro.')
+        }
+    }
+
+}
+
+export { UserRepository }
