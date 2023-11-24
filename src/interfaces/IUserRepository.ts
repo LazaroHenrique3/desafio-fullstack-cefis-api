09ddbdddbe55524264dcf7f21136de2d5ead6f14
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-24 16:20:53 -0300

    feat: Definindo a interface do repository de User

---
+++ src/interfaces/IUserRepository.ts
@@ -0,0 +1,10 @@
+import { User } from '@prisma/client'
+
+export interface IUserRepository {
+    list(page: number, limit: number, filter: string, orderBy: 'asc' | 'desc'): Promise<User[] | Error>
+    count(filter: string): Promise<number | Error>
+    getById(idUser: number): Promise<User | null | Error>
+    create(name: string, role: 'STUDENT' | 'TEACHER'): Promise<User | Error>
+    update(idUser: number, name: string): Promise<User | null | Error>
+    delete(idUser: number): Promise<void | null | Error>
+}
