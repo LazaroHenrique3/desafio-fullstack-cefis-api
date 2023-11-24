1ddcc3b9a1442affea94779cc91ffeb3ff70dff6
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-24 16:14:57 -0300

    feat: Adicionando as rotas de User

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,15 @@
 import { Router } from 'express'
 
+import { UserController } from './controller/user'
+
 const routes = Router()
 
 routes.get('/', (_, res) => res.send('Hello, world!'))
+
+routes.post('/createUser', UserController.createUser)
+routes.get('/listUser', UserController.listUser)
+routes.get('/listUser/:idUser', UserController.getUserById)
+routes.delete('/deleteUser/:idUser', UserController.deleteUser)
+routes.put('/updateUser/:idUser', UserController.updateUser)
 
 export { routes }
