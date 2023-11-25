ad538bc0005afde1f023a4a43d47fd31ea355c62
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-25 16:11:30 -0300

    feat: Criando a middleware de validação do createCourse

--- src/controller/course/Create.ts
+++ src/controller/course/Create.ts
@@ -1,17 +1,28 @@
 import { Request, Response } from 'express'
+import * as yup from 'yup'
 import { Course } from '@prisma/client'
 
 import { CreateCourseService } from '../../services/courseServices/CreateCourseService'
 import { CourseRepository } from '../../repositories/CourseRepository'
+import { validation } from '../../middlewares/Validation'
 
 //Para tipar o body do request
 interface IBodyProps extends Omit<Course, 'id'> { }
+
+//Midleware
+export const createCourseValidation = validation((getSchema) => ({
+    body: getSchema<IBodyProps>(yup.object().shape({
+        title: yup.string().required(),
+        teacherId: yup.number().integer().moreThan(0).required(),
+        duration: yup.number().integer().moreThan(0).required()
+    }))
+}))
 
 export const createCourse = async (request: Request<{}, {}, IBodyProps>, response: Response) => {
     const { title, duration, teacherId } = request.body
 
     const createCourse = new CreateCourseService(new CourseRepository())
-    const resultCourse = await createCourse.execute(title, duration, teacherId)
+    const resultCourse = await createCourse.execute(String(title), duration, teacherId)
 
     if (resultCourse instanceof Error) {
         return response.status(500).json({
