ed8ca7a47c95fb31a2d00677b55729a6a0fbe8b2
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-25 18:40:06 -0300

    feat: Criando a middleware de validação do createQuestion

--- src/controller/question/Create.ts
+++ src/controller/question/Create.ts
@@ -1,17 +1,27 @@
 import { Request, Response } from 'express'
+import * as yup from 'yup'
 import { Question } from '@prisma/client'
 
 import { CreateQuestionService } from '../../services/questionServices/CreateQuestionService'
 import { QuestionRepository } from '../../repositories/QuestionRepository'
+import { validation } from '../../middlewares/Validation'
 
 //Para tipar o body do request
 interface IBodyProps extends Omit<Question, 'id'> { }
+
+export const createQuestionValidation = validation((getSchema) => ({
+    body: getSchema<IBodyProps>(yup.object().shape({
+        question_text: yup.string().required(),
+        idCourse: yup.number().integer().required().moreThan(0),
+        idStudent: yup.number().integer().required().moreThan(0),
+    }))
+}))
 
 export const createQuestion = async (request: Request<{}, {}, IBodyProps>, response: Response) => {
     const { idCourse, idStudent, question_text } = request.body
 
     const createQuestion = new CreateQuestionService(new QuestionRepository())
-    const resultQuestion = await createQuestion.execute(question_text, idCourse, idStudent)
+    const resultQuestion = await createQuestion.execute(String(question_text), idCourse, idStudent)
 
     if (resultQuestion instanceof Error) {
         return response.status(500).json({
