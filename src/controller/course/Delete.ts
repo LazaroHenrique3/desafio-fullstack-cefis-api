2fd8054d370a9e24fcea3528acddaaaf8c08807f
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-25 16:12:09 -0300

    feat: Criando a middleware de validação do deleteCourse

--- src/controller/course/Delete.ts
+++ src/controller/course/Delete.ts
@@ -1,12 +1,20 @@
 import { Request, Response } from 'express'
+import * as yup from 'yup'
 
 import { DeleteCourseService } from '../../services/courseServices/DeleteCourseService'
 import { CourseRepository } from '../../repositories/CourseRepository'
+import { validation } from '../../middlewares/Validation'
 
 //Para tipar os params do request
 interface IParamProps {
     idCourse?: number
 }
+
+export const deleteCourseValidation = validation((getSchema) => ({
+    params: getSchema<IParamProps>(yup.object().shape({
+        idCourse: yup.number().integer().required().moreThan(0),
+    })),
+}))
 
 export const deleteCourse = async (request: Request<IParamProps>, response: Response) => {
     const { idCourse } = request.params
