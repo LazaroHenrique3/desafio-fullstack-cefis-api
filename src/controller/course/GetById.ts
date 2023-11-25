f8ebe33ba7a3f29e092ef6189986d8ac8d74a9b4
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-25 16:12:39 -0300

    feat: Criando a middleware de validação do getCourseById

--- src/controller/course/GetById.ts
+++ src/controller/course/GetById.ts
@@ -1,12 +1,20 @@
 import { Request, Response } from 'express'
+import * as yup from 'yup'
 
 import { GetCourseByIdService } from '../../services/courseServices/GetCourseById'
 import { CourseRepository } from '../../repositories/CourseRepository'
+import { validation } from '../../middlewares/Validation'
 
 //Para tipar os params do request
 interface IParamProps {
     idCourse?: number
 }
+
+export const getCourseByIdValidation = validation((getSchema) => ({
+    params: getSchema<IParamProps>(yup.object().shape({
+        idCourse: yup.number().integer().required().moreThan(0),
+    })),
+}))
 
 export const getCourseById = async (request: Request<IParamProps>, response: Response) => {
     const { idCourse } = request.params
