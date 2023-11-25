e8531b33c024f11a9afceb0de2e41fa056f480d2
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-25 17:06:50 -0300

    feat: Criando a middleware de validação do updateUser

--- src/controller/user/Update.ts
+++ src/controller/user/Update.ts
@@ -1,23 +1,36 @@
 import { Request, Response } from 'express'
+import * as yup from 'yup'
 import { User } from '@prisma/client'
 
 import { UpdateUserService } from '../../services/userServices/UpdateUserService'
 import { UserRepository } from '../../repositories/UserRepository'
+import { validation } from '../../middlewares/Validation'
 
 //Para tipar o body do request
 interface IBodyProps extends Omit<User, 'id'> { }
 
 //Para tipar os params do request
 interface IParamProps {
-    idUser: number
+    idUser?: number
 }
+
+//Middleware
+export const updateUserValidation = validation((getSchema) => ({
+    params: getSchema<IParamProps>(yup.object().shape({
+        idUser: yup.number().integer().required().moreThan(0),
+    })),
+    body: getSchema<IBodyProps>(yup.object().shape({
+        name: yup.string().required(),
+        role: yup.string().oneOf(['STUDENT', 'TEACHER']).required()
+    }))
+}))
 
 export const updateUser = async (request: Request<IParamProps, {}, IBodyProps>, response: Response) => {
     const { name } = request.body
     const { idUser } = request.params
 
     const updateUser = new UpdateUserService(new UserRepository())
-    const resultUpdateUser = await updateUser.execute(Number(idUser), name)
+    const resultUpdateUser = await updateUser.execute(Number(idUser), String(name))
 
     if(resultUpdateUser instanceof Error){
         return response.status(500).json({
