9008bf850bb0c051818311dc3dca1e206c12c0ae
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-25 11:37:39 -0300

    feat: Adicionando as rotas de questions e responses no arquivo de rotas principal

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,10 +2,14 @@ import { Router } from 'express'
 
 import { userRoutes } from './userRouter'
 import { courseRoutes } from './courseRouter'
+import { questionRoutes } from './questionRouter'
+import { responseRoutes } from './responseRoute'
 
 const routes = Router()
 
 routes.use('/users', userRoutes)
 routes.use('/courses', courseRoutes)
+routes.use('/questions', questionRoutes)
+routes.use('/responses', responseRoutes)
 
 export { routes }
