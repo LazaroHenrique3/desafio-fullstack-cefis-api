d849fc5dade5749083faa6002bf5479bd89c1745
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-25 18:38:58 -0300

    feat: Criando a middleware de validação do createResponse

--- src/controller/response/Create.ts
+++ src/controller/response/Create.ts
@@ -1,17 +1,26 @@
 import { Request, Response } from 'express'
+import * as yup from 'yup'
 import { Response as IResponse } from '@prisma/client'
 
 import { CreateResponseService } from '../../services/responseServices/CreateResponseService'
 import { ResponseRepository } from '../../repositories/ResponseRepository'
+import { validation } from '../../middlewares/Validation'
 
 //Para tipar o body do request
 interface IBodyProps extends Omit<IResponse, 'id'> { }
+
+export const createResponseValidation = validation((getSchema) => ({
+    body: getSchema<IBodyProps>(yup.object().shape({
+        response_text: yup.string().required(),
+        idQuestion: yup.number().integer().required().moreThan(0),
+    }))
+}))
 
 export const createResponse = async (request: Request<{}, {}, IBodyProps>, response: Response) => {
     const { idQuestion, response_text } = request.body
 
     const createResponse = new CreateResponseService(new ResponseRepository())
-    const resultResponse = await createResponse.execute(response_text, idQuestion)
+    const resultResponse = await createResponse.execute(String(response_text), idQuestion)
 
     if (resultResponse instanceof Error) {
         return response.status(500).json({
