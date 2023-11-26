4dfe96c4428015fe1cbcbb9e39ba084e2ddac971
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-26 09:59:49 -0300

    refactor: Adicionando conversão de tipos no controller de listResponsesByIdQuestion e createUser  para garantir a tipagem correta

--- src/controller/response/ListResponsesByIdQuestion.ts
+++ src/controller/response/ListResponsesByIdQuestion.ts
@@ -41,7 +41,7 @@ export const ListResponsesByIdQuestion = async (request: Request<IParamProps, {}
     const resultResponses = await listResponses.execute(
         Number(page) || DefaultQueryParams.DEFAULT_PAGE, 
         Number(limit) || DefaultQueryParams.DEFAULT_LIMIT, 
-        orderBy as 'asc' | 'desc' || DefaultQueryParams.DEFAULT_ORDER_BY,
+        String(orderBy) as 'asc' | 'desc' || DefaultQueryParams.DEFAULT_ORDER_BY,
         Number(idQuestion)
     )
 
--- src/controller/user/Create.ts
+++ src/controller/user/Create.ts
@@ -21,7 +21,7 @@ export const createUser = async (request: Request<{}, {}, IBodyProps>, response:
     const { name, role } = request.body
 
     const createUser = new CreateUserService(new UserRepository())
-    const resultUser = await createUser.execute(String(name), role as 'STUDENT' | 'TEACHER')
+    const resultUser = await createUser.execute(String(name), String(role) as 'STUDENT' | 'TEACHER')
 
     if (resultUser instanceof Error) {
         return response.status(500).json({
