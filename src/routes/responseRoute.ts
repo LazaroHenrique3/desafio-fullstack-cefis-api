0e54af17394a5a98e96a5bd8f2ddd80d89118016
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-26 10:08:47 -0300

    feat: Adicionando o parâmetro idTeacher na rota de createResponse

--- src/routes/responseRoute.ts
+++ src/routes/responseRoute.ts
@@ -4,7 +4,7 @@ import { ResponseController } from '../controller/response'
 
 const responseRoutes = Router()
 
-responseRoutes.post('/createResponse', ResponseController.createResponseValidation, ResponseController.createResponse)
+responseRoutes.post('/createResponse/:idTeacher', ResponseController.createResponseValidation, ResponseController.createResponse)
 responseRoutes.get('/listResponseQuestion/:idQuestion', ResponseController.listResponsesByIdQuestionValidation, ResponseController.ListResponsesByIdQuestion)
 
 export { responseRoutes }
