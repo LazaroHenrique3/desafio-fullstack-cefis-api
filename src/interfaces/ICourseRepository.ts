fa3083644e2181eae4ba31de44dbccd969cb0af3
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-26 10:13:19 -0300

    refactor: Ajustando os parâmetro de retorno das funções de ICourseRepository

--- src/interfaces/ICourseRepository.ts
+++ src/interfaces/ICourseRepository.ts
@@ -4,7 +4,7 @@ export interface ICourseRepository {
     list(page: number, limit: number, filter: string, orderBy: 'asc' | 'desc'): Promise<Course[] | Error>
     count(filter: string): Promise<number | Error>
     getById(idCourse: number): Promise<Course | null | Error>
-    create(title: string, duration: number, teacherId: number): Promise<Course | null | Error>
-    update(idCourse: number, title: string, duration: number): Promise<Course | null | Error>
-    delete(idCourse: number): Promise<void | null | Error>
+    create(title: string, duration: number, teacherId: number): Promise<Course  | Error>
+    update(idCourse: number, title: string, duration: number): Promise<Course | Error>
+    delete(idCourse: number): Promise<void | Error>
 }
