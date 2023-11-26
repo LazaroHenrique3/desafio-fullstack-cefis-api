c0761766880692c70488039b1008455169b78e75
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-26 10:04:53 -0300

    refactor: Removendo temporiariamente algumas funções das interfaces de IQuestionRepository e IResponseRepository que não serão utilizados no contexto solicitado no desafio

--- src/interfaces/IResponseRepository.ts
+++ src/interfaces/IResponseRepository.ts
@@ -3,8 +3,5 @@ import { Response } from '@prisma/client'
 export interface IResponseRepository {
     listByIdQuestion(page: number, limit: number, orderBy: 'asc' | 'desc', idQuestion: number): Promise<Response[] | Error>
     count(idQuestion: number): Promise<number | Error>
-    getById(idResponse: number): Promise<Response | null | Error>
-    create(responseText: string, idQuestion: number): Promise<Response | null | Error>
-    update(idResponse: number, responseText: string): Promise<Response | null | Error>
-    delete(idResponse: number): Promise<void | null | Error>
+    create(responseText: string, idQuestion: number): Promise<Response | Error>
 }
