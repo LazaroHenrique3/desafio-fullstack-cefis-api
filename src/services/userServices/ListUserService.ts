97fd9d554325658d4dc8fac8e6912d1455e1f429
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-28 21:41:51 -0300

    feat: Adicionando o filtro de de typeUser no listUser

--- src/services/userServices/ListUserService.ts
+++ src/services/userServices/ListUserService.ts
@@ -3,8 +3,8 @@ import { IUserRepository } from '../../interfaces/IUserRepository'
 class ListUserService {
     constructor (private UserRepository: IUserRepository) {}
 
-    public async execute(page: number, limit: number, filter: string, orderBy: 'asc' | 'desc') {
-        const user = await this.UserRepository.list(page, limit, filter, orderBy)
+    public async execute(page: number, limit: number, filter: string, orderBy: 'asc' | 'desc', typeUser: 'STUDENT' | 'TEACHER' | '') {
+        const user = await this.UserRepository.list(page, limit, filter, orderBy, typeUser)
         return user
     }
 }
