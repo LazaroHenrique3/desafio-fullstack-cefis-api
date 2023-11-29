43d747196f64deb404a85ef12a530a5e2a0c7887
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-28 21:45:53 -0300

    feat: Preparando o list da interface IUserRepository para receber a novo parâmetro de pesquisa typeUser

--- src/interfaces/IUserRepository.ts
+++ src/interfaces/IUserRepository.ts
@@ -2,7 +2,7 @@ import { User } from '@prisma/client'
 import { CustomError } from '../errors/CustomErrors'
 
 export interface IUserRepository {
-    list(page: number, limit: number, filter: string, orderBy: 'asc' | 'desc'): Promise<User[] | CustomError>
+    list(page: number, limit: number, filter: string, orderBy: 'asc' | 'desc', typeUser: 'STUDENT' | 'TEACHER' | ''): Promise<User[] | CustomError>
     count(filter: string): Promise<number | CustomError>
     getById(idUser: number): Promise<User | null | CustomError>
     create(name: string, role: 'STUDENT' | 'TEACHER'): Promise<User | CustomError>
