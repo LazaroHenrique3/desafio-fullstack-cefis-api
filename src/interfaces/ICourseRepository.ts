b7863d822075b76a056bff4759813c860713ca4f
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-29 13:42:56 -0300

    refactor: Ajustando a interface de resposta do getById de Course

--- src/interfaces/ICourseRepository.ts
+++ src/interfaces/ICourseRepository.ts
@@ -1,10 +1,17 @@
-import { Course } from '@prisma/client'
+import { Course, Question } from '@prisma/client'
 import { CustomError } from '../errors/CustomErrors'
+
+export interface IGetCourseByIdResponse extends Course {
+    teacher: {
+        name: string
+    }
+    Question: Question[]
+}
 
 export interface ICourseRepository {
     list(page: number, limit: number, filter: string, orderBy: 'asc' | 'desc'): Promise<Course[] | CustomError>
     count(filter: string): Promise<number | CustomError>
-    getById(idCourse: number): Promise<Course | null | CustomError>
+    getById(idCourse: number): Promise<IGetCourseByIdResponse | null | CustomError>
     create(title: string, duration: number, teacherId: number): Promise<Course  | CustomError>
     update(idCourse: number, title: string, duration: number): Promise<Course | CustomError>
     delete(idCourse: number): Promise<void | CustomError>
