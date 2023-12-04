60148944391a1ffb45580932b1d6696ee122dc77
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-12-03 22:59:17 -0300

    feat: Adicionando a função updateWithoutPassword, e ajustando a interface de retorno das demais,  impedindo o retorno de password

--- src/interfaces/IUserRepository.ts
+++ src/interfaces/IUserRepository.ts
@@ -1,12 +1,15 @@
 import { User } from '@prisma/client'
 import { CustomError } from '../errors/CustomErrors'
 
+export interface IUserWithoutPassword extends Omit<User, 'password'>  {}
+
 /* Representa o coontrato que o repository deve seguir no momento de sua implementação independente do ORM */
 export interface IUserRepository {
-    list(page: number, limit: number, filter: string, orderBy: 'asc' | 'desc', typeUser: 'STUDENT' | 'TEACHER' | ''): Promise<User[] | CustomError>
+    list(page: number, limit: number, filter: string, orderBy: 'asc' | 'desc', typeUser: 'STUDENT' | 'TEACHER' | ''): Promise<IUserWithoutPassword[] | CustomError>
     count(filter: string): Promise<number | CustomError>
     getById(idUser: number): Promise<User | null | CustomError>
-    create(name: string, role: 'STUDENT' | 'TEACHER'): Promise<User | CustomError>
-    update(idUser: number, name: string): Promise<User | CustomError>
+    create(name: string, email: string, password: string, role: 'STUDENT' | 'TEACHER'): Promise<User | CustomError>
+    update(idUser: number, name: string, email: string, password: string): Promise<User | CustomError>
+    updateWithoutPassword(idUser: number, name: string, email: string): Promise<User | CustomError>
     delete(idUser: number): Promise<void | CustomError>
 }
