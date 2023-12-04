62f2762161abe3fcd1573dce77822615f699eef5
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-12-04 10:03:10 -0300

    feat: Adicionando a definição de getByEmail em IUserRepository

--- src/interfaces/IUserRepository.ts
+++ src/interfaces/IUserRepository.ts
@@ -8,6 +8,7 @@ export interface IUserRepository {
     list(page: number, limit: number, filter: string, orderBy: 'asc' | 'desc', typeUser: 'STUDENT' | 'TEACHER' | ''): Promise<IUserWithoutPassword[] | CustomError>
     count(filter: string): Promise<number | CustomError>
     getById(idUser: number): Promise<User | null | CustomError>
+    getByEmail(email: string): Promise<User | null | CustomError>
     create(name: string, email: string, password: string, role: 'STUDENT' | 'TEACHER'): Promise<User | CustomError>
     update(idUser: number, name: string, email: string, password: string): Promise<User | CustomError>
     updateWithoutPassword(idUser: number, name: string, email: string): Promise<User | CustomError>
