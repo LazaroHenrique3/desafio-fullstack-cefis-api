48a3a0c33e95f0381c736dbb9e0a99dc55e9a26c
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-12-03 23:10:51 -0300

    feat: Removendo o retorno da password no GetUserByIdService

--- src/services/userServices/GetUserById.ts
+++ src/services/userServices/GetUserById.ts
@@ -1,11 +1,20 @@
+import { CustomError } from '../../errors/CustomErrors'
 import { IUserRepository } from '../../interfaces/IUserRepository'
 
 class GetUserByIdService {
-    constructor (private UserService: IUserRepository) {}
+    constructor(private UserService: IUserRepository) { }
 
-    public async execute (idUser: number) {
+    public async execute(idUser: number) {
         const user = await this.UserService.getById(idUser)
-        return user
+
+        //Caso for erro eu retorno normalmente
+        if (user instanceof CustomError) return user
+
+        //Caso contrário eu preciso remover a propriedade de password no retorno
+        //Optei fazer aqui, pois considero regra de negócio
+        // eslint-disable-next-line 
+        const { password: _, ...userWithoutPassword } = user
+        return userWithoutPassword
     }
 }
 
