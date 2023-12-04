6df77f3820cd50c7c9e44d85d8da06f3e77b4a19
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-12-03 23:18:24 -0300

    feat: Nova implementação do UpdateUserService, incorporando email e password

--- src/services/userServices/UpdateUserService.ts
+++ src/services/userServices/UpdateUserService.ts
@@ -1,19 +1,49 @@
 import { IUserRepository } from '../../interfaces/IUserRepository'
-import { checkIfUserExists } from '../utils/checkFunctions'
+import { 
+    checkIfUserEmailAlreadyExistsInUpdate, 
+    checkIfUserExists 
+} from '../utils/checkFunctions'
 import { CustomError } from '../../errors/CustomErrors'
+import { PasswordCrypto } from '../../externalServices/PasswordCrypto'
+import { User } from '@prisma/client'
 
 class UpdateUserService {
     constructor(private UserRepository: IUserRepository) { }
 
-    public async execute(idUser: number, name: string) {
+    public async execute(idUser: number, name: string, email: string, password: string) {
         //Verificando se o user existe
         const userExists = await checkIfUserExists(idUser)
         if (!userExists) {
             return new CustomError('Usuário não encontrado.', 404)
         }
 
-        const updatedUser = await this.UserRepository.update(idUser, name)
-        return updatedUser
+        //Verificando se o email já foi cadastrado
+        const emailExists = await checkIfUserEmailAlreadyExistsInUpdate(email, idUser)
+        if (emailExists) {
+            return new CustomError('Este email já esta em uso.', 409)
+        }
+
+        let updatedUser: User | CustomError
+
+        //Significa que foi passado senha para alteração
+        if (password !== '' &&  password !== undefined) {
+            //Criptografando a senha utilizando bcrypt
+            const hashedPassword = await PasswordCrypto.hashPassword(password)
+
+            updatedUser = await this.UserRepository.update(idUser, name, email, hashedPassword)
+        }
+
+        //Se chegou aqui significa que não foi passado senha para alteração
+        updatedUser = await this.UserRepository.updateWithoutPassword(idUser, name, email)
+
+        //Caso for erro eu retorno normalmente
+        if (updatedUser instanceof CustomError) return updatedUser
+
+        //Caso contrário eu preciso remover a propriedade de password no retorno
+        //Optei fazer aqui, pois considero regra de negócio
+        // eslint-disable-next-line 
+        const { password: _, ...userWithoutPassword } = updatedUser
+        return userWithoutPassword
     }
 }
 
