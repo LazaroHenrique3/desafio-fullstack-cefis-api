f99c0c771861befb60f3bb7a7a52cc96e2fd3292
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-12-03 22:43:07 -0300

    feat: Adicionando email e password no controller de  createUser

--- src/controller/user/Create.ts
+++ src/controller/user/Create.ts
@@ -14,15 +14,17 @@ interface IBodyProps extends Omit<User, 'id'> { }
 export const createUserValidation = validation((getSchema) => ({
     body: getSchema<IBodyProps>(yup.object().shape({
         name: yup.string().required(),
+        email: yup.string().required().email().min(5).max(100),
+        password: yup.string().required().min(6),
         role: yup.string().oneOf(['STUDENT', 'TEACHER']).required()
     }))
 }))
 
 export const createUser = async (request: Request<{}, {}, IBodyProps>, response: Response) => {
-    const { name, role } = request.body
+    const { name, email, password, role } = request.body
 
     const createUser = new CreateUserService(new UserRepository())
-    const resultUser = await createUser.execute(String(name), String(role) as 'STUDENT' | 'TEACHER')
+    const resultUser = await createUser.execute(String(name), String(email), String(password), String(role) as 'STUDENT' | 'TEACHER')
 
     if (resultUser instanceof CustomError) {
         return response.status(resultUser.status).json({
